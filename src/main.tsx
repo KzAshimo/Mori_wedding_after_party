c9f78094939a08fb5c45dd566aca3d5f5b2337ff
KzAshimo <good.ashimo@gmail.com>
2025-06-27 23:22:42 +0900

    css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';           // ゲスト登録画面（例）
 import Admin from './admin';       // 管理者画面
